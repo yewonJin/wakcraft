de0408a899e9c4bc28c93a03e6552c382a1b2a84
yewonJin <jyw966@naver.com>
2025-04-01 02:06:24 +0900

    feat: 예전에 있었던 티어 타입 추가

--- packages/types/src/tier.ts
+++ packages/types/src/tier.ts
@@ -1,4 +1,8 @@
-import { TIER, TIER_LIST } from "@repo/constants";
+import { TIER, TIER_GROUP, ALL_TIER_GROUP, ALL_TIER } from '@repo/constants'
 
-export type TierGroup = keyof typeof TIER_LIST;
-export type Tier = (typeof TIER)[number];
+export type TierGroup = keyof typeof TIER_GROUP
+export type Tier = (typeof TIER)[number]
+export type AllTierGroup = keyof typeof ALL_TIER_GROUP
+export type AllTier = (typeof ALL_TIER)[number]
+
+export type Line = '눕' | '계륵' | '프로' | '국밥' | '해커'
