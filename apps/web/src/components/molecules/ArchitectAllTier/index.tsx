8f1f9d4c11dc42ac1d0aeab6f33d0c6cc49ec4bc
yewonJin <jyw966@naver.com>
2025-04-02 15:15:08 +0900

    feat: 티어 없을 시, 컴포넌트 렌더링하지 않기

--- apps/web/src/components/molecules/ArchitectAllTier/index.tsx
+++ apps/web/src/components/molecules/ArchitectAllTier/index.tsx
@@ -7,12 +7,13 @@ type Props = {
 }
 
 export default function ArchitectAllTier({ tier }: Props) {
+  if (!tier.filter((item) => item.result !== '언랭').length) return null
+
   return (
     <div className="mb-6 flex flex-wrap gap-2 px-4 xl:px-0">
-      {tier.map((item) => {
-        if (item.result === '언랭') return null
-
-        return (
+      {tier
+        .filter((item) => item.result !== '언랭')
+        .map((item) => (
           <div
             key={item.season}
             className={cn(
@@ -26,8 +27,7 @@ export default function ArchitectAllTier({ tier }: Props) {
               className={cn('font-medium', getTierTextColor(item.result))}
             >{`${item.result}`}</span>
           </div>
-        )
-      })}
+        ))}
     </div>
   )
 }
