1c6b3f3d6ed97153173031a4166c7aa3a4d64c22
yewonJin <jyw966@naver.com>
2025-04-08 16:10:35 +0900

    refactor: TierBox 컴포넌트에서 TIER_IMAGES 상수를 별도 파일로 이동

--- apps/web/src/components/atoms/TierBox/index.tsx
+++ apps/web/src/components/atoms/TierBox/index.tsx
@@ -1,9 +1,28 @@
-import { TIER_IMAGES } from '@repo/constants'
 import { AllTier } from '@repo/types'
 
 type Props = {
   tier: AllTier
 }
+
+export const TIER_IMAGES: { [key in AllTier]: string } = {
+  마카게: "url('/images/makage.webp')",
+  오마카세: "url('/images/omakase.webp')",
+  해커: "url('/images/hacker.webp')",
+  해장국: "url('/images/haejangguk.webp')",
+  국밥: "url('/images/gukbap.webp')",
+  '미지근한 국밥': "url('/images/miguk.webp')",
+  '식은 국밥': "url('/images/miguk.webp')",
+  프로: "url('/images/pro.webp')",
+  계추: "url('/images/gyechu.webp')",
+  계륵: "url('/images/gyeruik.webp')",
+  '가짜 눕': "url('/images/noob.webp')",
+  '착한 눕': "url('/images/noob.webp')",
+  '안 나쁜 눕': "url('/images/noob.webp')",
+  '그냥 눕': "url('/images/purenoob.webp')",
+  '진짜 눕': "url('/images/purenoob.webp')",
+  '퓨어 눕': "url('/images/purenoob.webp')",
+  언랭: "url('/images/purenoob.webp')",
+} as const
 
 export default function TierBox({ tier }: Props) {
   return (
